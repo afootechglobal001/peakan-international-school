80f8e1ccdc26d3f10403182bf1802825db446362
Mike Afolabi <afolabi.o@eleadernetwork.com>
2025-10-26 20:35:58 +0100

    update on logo and seo flyer

--- api/dev/config/connection.php
+++ api/dev/config/connection.php
@@ -6,15 +6,15 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header('Content-Type: application/json; charset=UTF-8');
 
 ////////////for live connect  
-$_HOST_NAME = "131.153.147.42";  
-$_DB_USERNAME ="afootec1_admin";
-$_DB_PASSWORD ="AfooTECH@2023";
+$_HOST_NAME = "145.239.185.59";  
+$_DB_USERNAME ="peakan";
+$_DB_PASSWORD ="Password@12345!";
 
 // $_HOST_NAME = "localhost";  
 // $_DB_USERNAME ="root";
 // $_DB_PASSWORD ="";
 $conn = mysqli_connect($_HOST_NAME, $_DB_USERNAME, $_DB_PASSWORD)or die("Unable to connect to MySQL1");
-mysqli_select_db($conn,"afootec1_peakan");
+mysqli_select_db($conn,"peakan_db");
 /////////////////////////////////////////////////////////////////
 ?>
 
